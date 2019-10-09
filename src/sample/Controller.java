e6d09e9ebe10787fa87567629b430ad957bf6c87
Gl3Bbl4 <borisenko.gleb.work@gmail.com>
2019-10-10 00:32:39 +0400

    v1.0

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,6 +2,7 @@ package sample;
 
 import java.io.File;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -102,6 +103,23 @@ public class Controller {
 
     }
 
+
+    void checkTrap() {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        if ((grid[0][2] == player && grid[1][1] == player && grid[2][0] == bot) ||
+                (grid[0][2] == bot && grid[1][1] == player && grid[2][0] == player)
+        ) {
+            arrayList.add(0);
+            arrayList.add(8);
+            doMove(getRandomButtonFromList(true, arrayList));
+        } else if ((grid[0][0] == player && grid[1][1] == player && grid[2][2] == bot) ||
+                (grid[0][0] == bot && grid[1][1] == player && grid[2][2] == player)) {
+            arrayList.add(2);
+            arrayList.add(6);
+            doMove(getRandomButtonFromList(true, arrayList));
+        }
+    }
+
     void start() {
 
         if (btPlay.getText().equals("Заново")) {
@@ -180,15 +198,23 @@ public class Controller {
         else {
             switch (step) {
                 case 0:
+                    //если игрок выбрал О
                     if (checkAllZero()) {
                         doMove(bt5);
                         step = 1;
-                    } else if (grid[1][1] == player) {
+                    }
+                    //если игрок Х поставил в центр, то ставить в угол
+                    else if (grid[1][1] == player) {
                         Button bt = getRandomButtonFromList(true, null);
                         doMove(bt);
-                        step = 3;
-                    } else {
+                        step = 2;
+                    }
+                    //если игрок Х поставил не в центр, то занять центр
+                    else if (getLastSide() == null) {
                         doMove(bt5);
+                        step = 4;
+                    } else {
+                        doMove(getRandomButtonFromList(true, getLastSide()));
                         step = 2;
                     }
                     break;
@@ -198,61 +224,105 @@ public class Controller {
                     step = 2;
                     break;
                 case 2:
-                    int lastPlaces = checkLastPlaces(bot);
-
-                        doMove((Button) gridPane.getChildren().get(lastPlaces));
-
+                    if (grid[1][1] != 0) {
+                        int lastPlaces = checkLastPlaces(bot);
+                        if (lastPlaces != -1) {
+                            doMove((Button) gridPane.getChildren().get(lastPlaces));
+                        }
+                    } else {
+                        doMove(bt5);
+                    }
                     break;
+                case 3:
+                    checkTrap();
+                    step = 2;
+                    break;
+                case 4:
+                    checkTrap();
+
+                    doMove(getRandomButtonFromList(false, null));
+                    step = 2;
+                    break;
+
             }
         }
     }
 
+
+    ArrayList getLastSide() {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == player && !(i == 1 && j == 1) && (i * 3 + j) % 2 == 1) {
+
+                    if (i == 0) {
+                        arrayList.add(0);
+                        arrayList.add(2);
+                    } else if (i == 1 && j == 0) {
+                        arrayList.add(0);
+                        arrayList.add(6);
+                    } else if (i == 1 && j == 2) {
+                        arrayList.add(2);
+                        arrayList.add(8);
+                    } else {
+                        arrayList.add(6);
+                        arrayList.add(8);
+                    }
+                    return arrayList;
+                }
+            }
+        }
+        return null;
+    }
+
     int checkLastPlaces(byte X_O) {
 
-        byte[] arr = new byte[8];
+        ArrayList<Integer> arr = new ArrayList<Integer>();
         byte k = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                if ((grid[i][j] == 0 && grid[i][(j + 1) % 3] == 0 && (grid[i][(j + 2) % 3] == X_O || grid[i][(j + 2) % 3] == 0)) ||
-                        (grid[i][j] == 0 && grid[(i + 1) % 3][j] == 0 && (grid[(i + 2) % 3][j] == X_O || grid[(i + 2) % 3][j] == 0)) ||
-                        (i == j && grid[i][j] == 0 && grid[(i + 1) % 3][(j + 1) % 3] == 0 && (grid[(i + 2) % 3][(j + 2) % 3] == X_O || grid[(i + 2) % 3][(j + 2) % 3] == 0)) ||
-                        (((i == 0 && j == 2) || (i == j) || (i == 2 && j == 0)) && grid[i][j] == 0 && grid[(i + 1) % 3][(j + 2) % 3] == 0 && (grid[(i + 2) % 3][(j + 4) % 3] == X_O || grid[(i + 2) % 3][(j + 4) % 3] == 0))) {
+                if ((grid[i][j] == 0 && grid[i][(j + 1) % 3] == 0 && grid[i][(j + 2) % 3] == X_O) ||
+                        (grid[i][j] == 0 && grid[(i + 1) % 3][j] == 0 && grid[(i + 2) % 3][j] == X_O) ||
+                        (i == j && grid[i][j] == 0 && grid[(i + 1) % 3][(j + 1) % 3] == 0 && grid[(i + 2) % 3][(j + 2) % 3] == X_O) ||
+                        (((i == 0 && j == 2) || (i == j) || (i == 2 && j == 0)) && grid[i][j] == 0 && grid[(i + 1) % 3][(j + 2) % 3] == 0 && grid[(i + 2) % 3][(j + 4) % 3] == X_O)) {
                     return i * 3 + j;
                 } else if (grid[i][j] == 0) {
-                    arr[k] = (byte) (i * 3 + j);
+                    arr.add(i * 3 + j);
                     k++;
                 }
             }
         }
-
-
-        return gridPane.getChildren().get(getRandomButtonFromList(true, arr) );
+        if (k != 0) {
+            Button bt = getRandomButtonFromList(true, arr);
+            doMove(bt);
+        }
+        return -1;
     }
 
     Button makeTrap(byte X_0) {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == X_0 && (i * 3 + j) % 2 == 0) {
-                    byte[] arrConnor = new byte[2];
-                    if (i == 0) arrConnor[0] = (byte) (((i + 2) % 3) * 3 + j);
-                    else arrConnor[0] = (byte) (((i + 1) % 3) * 3 + j);
-                    if (j == 0) arrConnor[1] = (byte) (((j + 2) % 3) + 3 * i);
-                    else arrConnor[1] = (byte) (((j + 1) % 3) + 3 * i);
+                    ArrayList<Integer> arrConnor = new ArrayList<>();
+                    if (i == 0) arrConnor.add(((i + 2) % 3) * 3 + j);
+                    else arrConnor.add(((i + 1) % 3) * 3 + j);
+                    if (j == 0) arrConnor.add(((j + 2) % 3) + 3 * i);
+                    else arrConnor.add(((j + 1) % 3) + 3 * i);
                     return getRandomButtonFromList(true, arrConnor);
                 } else if (grid[i][j] == X_0) {
-                    byte[] arrSide = new byte[2];
+                    ArrayList<Integer> arrSide = new ArrayList<Integer>();
                     if (i == 0) {
-                        arrSide[0] = (byte) 8;
-                        arrSide[1] = (byte) 6;
+                        arrSide.add(8);
+                        arrSide.add(6);
                     } else if (i == 1 && j == 0) {
-                        arrSide[0] = (byte) 2;
-                        arrSide[1] = (byte) 8;
+                        arrSide.add(2);
+                        arrSide.add(8);
                     } else if (i == 1 && j == 2) {
-                        arrSide[0] = (byte) 0;
-                        arrSide[1] = (byte) 6;
+                        arrSide.add(0);
+                        arrSide.add(6);
                     } else {
-                        arrSide[0] = (byte) 0;
-                        arrSide[1] = (byte) 2;
+                        arrSide.add(0);
+                        arrSide.add(2);
                     }
                     return getRandomButtonFromList(true, arrSide);
                 }
@@ -261,11 +331,10 @@ public class Controller {
         return null;
     }
 
-    Button getRandomButtonFromList(boolean true_conor, byte[] arr) {
-        byte i = (byte) (Math.random() * 3);
-        byte j = (byte) (Math.random() * 3);
-
-        if (arr == null) {
+    Button getRandomButtonFromList(boolean true_conor, ArrayList<Integer> arrayList) {
+        if (arrayList == null) {
+            byte i = (byte) (Math.random() * 3);
+            byte j = (byte) (Math.random() * 3);
             if (true_conor == true) {
                 if (i != 1 && j != 1 && grid[i][j] == 0) {
                     return (Button) gridPane.getChildren().get(i * 3 + j);
@@ -281,13 +350,9 @@ public class Controller {
             }
         } else {
             byte k;
-            do {
-                k = (byte) (Math.random() * arr.length);
-            }
-            while (arr[k] != 0);
-            return (Button) gridPane.getChildren().get(arr[k]);
+            k = (byte) (Math.random() * arrayList.size());
+            return (Button) gridPane.getChildren().get(arrayList.get(k));
         }
-
     }
 
     String checkEnd() {
@@ -339,7 +404,7 @@ public class Controller {
                 if ((grid[i][j] == 0 && grid[i][(j + 1) % 3] == X_O && grid[i][(j + 2) % 3] == X_O) ||
                         (grid[i][j] == 0 && grid[(i + 1) % 3][j] == X_O && grid[(i + 2) % 3][j] == X_O) ||
                         (i == j && grid[i][j] == 0 && grid[(i + 1) % 3][(j + 1) % 3] == X_O && grid[(i + 2) % 3][(j + 2) % 3] == X_O) ||
-                        (((i == 0 && j == 2) || (i == j) || (i == 2 && j == 0)) && grid[i][j] == 0 && grid[(i + 1) % 3][(j + 2) % 3] == X_O && grid[(i + 2) % 3][(j + 4) % 3] == X_O)) {
+                        (((i == 0 && j == 2) || (i == 1 && j == 1) || (i == 2 && j == 0)) && grid[i][j] == 0 && grid[(i + 1) % 3][(j + 2) % 3] == X_O && grid[(i + 2) % 3][(j + 4) % 3] == X_O)) {
                     return i * 3 + j;
                 }
             }
